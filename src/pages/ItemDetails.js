d58cefd67ceacd07d29191c04cd569283b7a982a
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-02-01 10:23:24 +0000

    fix problem with a link

--- src/pages/ItemDetails.js
+++ src/pages/ItemDetails.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button, Modal } from "react-bootstrap";
 
-function ItemDetails() {
+function ItemDetails({deliveryId}) {
   const storedToken = localStorage.getItem("authToken");
   const [item, setItem] = useState(null);
   const [show, setShow] = useState(false);
@@ -41,42 +41,49 @@ function ItemDetails() {
       .catch((err) => console.log(err));
   };
 
+  
+
   return (
     <div>
       {item && (
         <>
+
+          {/* Item Details */}
           <h1>{item.code}</h1>
           <p>Name: {item.name}</p>
           <p>Address: {item.address}</p>
           <p>Result: {item.result}</p>
-          <Link to={`/items/edit/${itemId}`}><Button>Change result</Button></Link>
+          <Link to={`/items/edit/${itemId}`}>
+            <Button>Change result</Button>
+          </Link>
+
+          <hr />
+
+          {/* Nav Links */}
+          <Link to={`/deliveries/${item.delivery._id}`}>
+            <Button>Back to list of items</Button>
+          </Link>
+          <Button variant="danger" onClick={handleShow}>
+            Delete item
+          </Button>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Are you sure?</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Please confirm that you want to delete this item.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Back
+              </Button>
+              <Button variant="danger" onClick={deleteItem}>
+                Delete this item
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </>
       )}
-
-      <hr />
-
-      <Link to={`/deliveries/`}>
-        <Button>Back to list of items</Button>
-      </Link>
-      <Button variant="danger" onClick={handleShow}>
-        Delete item
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Are you sure?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Please confirm that you want to delete this item.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Back
-          </Button>
-          <Button variant="danger" onClick={deleteItem}>
-            Delete this item
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </div>
   );
 }
